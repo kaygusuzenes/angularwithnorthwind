7654fdb2552694e67d6cd88fd37b43efe95ed01e
kaygusuzenes <eneskygsz@gmail.com>
2021-11-16 10:05:25 +0300

    compenents and services added

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded = false;
+  filterText = "";
+
+  constructor(private productService: ProductService,
+    private activatedRoute: ActivatedRoute, private toastrService: ToastrService) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      if (params["categoryId"]) {
+        this.getProductsByCategory(params["categoryId"])
+      } else {
+        this.getProducts()
+      }
+    })
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(response => {
+      this.products = response.data
+      this.dataLoaded = true;
+    })
+  }
+
+  getProductsByCategory(categoryId: number) {
+    this.productService.getProductsByCategory(categoryId).subscribe(response => {
+      this.products = response.data
+      this.dataLoaded = true;
+    })
+  }
+
+  addToCart(product: Product) {
+    if (product.unitsInStock === 0) {
+      this.toastrService.error("Ürüne ait stok bulunamadı.", product.productName)
+    }
+    else{
+      this.toastrService.success("Sepete başarıyla eklendi.", product.productName)
+    }
+  }
+
+}
